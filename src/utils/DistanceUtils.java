1b37d4d6c08e8b767050eb8107eb4230e7ed7f1a
Alexdef74307 <arabilloud@hotmail.fr>
2016-04-01 09:00:13 +0200

    Implémentation calcul distance

---
+++ src/utils/DistanceUtils.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import static java.lang.Math.*;
+
+/**
+ *
+ * @author Alexdef74307
+ */
+public class DistanceUtils {
+    
+    private static final Double earthRadius = 6371000.0;
+    
+    public static Double calculateDistance(Double longitudeAgency, Double latitudeAgency, 
+            Double longitudeTC, Double latitudeTC) {
+        
+        Double d = earthRadius * acos(sin(toRadians(latitudeAgency)) * sin(toRadians(latitudeTC)) + cos(toRadians(latitudeAgency)) * 
+                cos(toRadians(latitudeTC)) * cos(toRadians(longitudeAgency) - toRadians(longitudeTC)));
+        
+         // $query .= '6371000 * ACOS(SIN(RADIANS(:userLatitude)) * SIN(RADIANS(lat)) + COS(RADIANS(:userLatitude)) * COS(RADIANS(lat)) * COS(RADIANS(:userLongitude) - RADIANS(lng))) AS distance ';
+        
+        return d;
+    }
+    
+}
---
+++ test/testDistance.java
@@ -0,0 +1,24 @@
+
+import utils.DistanceUtils;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alexdef74307
+ */
+public class testDistance {
+    
+    public static void main(String[] args) {
+        
+        Double d = DistanceUtils.calculateDistance(-0.75, 45.45, 3.71667, 44.8167);
+        
+        System.out.println(d);
+        
+    }
+    
+}
